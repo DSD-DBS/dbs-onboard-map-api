361b62e927dd4810bed14c103a8a47181948a94f
Volodymyr Bezkorovainyi <vladimir.bezkorovainy@gmail.com>
2023-06-29 12:04:48 +0200

    coverage folder changed

--- scripts/code_coverage.sh
+++ scripts/code_coverage.sh
@@ -3,8 +3,9 @@
 # SPDX-License-Identifier: CC0-1.0
 
 DBS_ONBOARD_MAP_API_DIR=`pwd`
-COVERAGE_DIR=$DBS_ONBOARD_MAP_API_DIR/build/coverage
+# COVERAGE_DIR=$DBS_ONBOARD_MAP_API_DIR/build/coverage
+COVERAGE_DIR=$DBS_ONBOARD_MAP_API_DIR
 EXCLUDE='.*tests/|.*externals/|.*build/|.*download/'
 
-rm -rf $COVERAGE_DIR && mkdir -p $COVERAGE_DIR
+# rm -rf $COVERAGE_DIR && mkdir -p $COVERAGE_DIR
 gcovr -r $DBS_ONBOARD_MAP_API_DIR --exclude $EXCLUDE --html --html-details -o $COVERAGE_DIR/coverage.html
