8047f0abc52344bb3be6563993d4b39829c43b2f
Volodymyr Bezkorovainyi <vladimir.bezkorovainy@gmail.com>
2023-06-29 12:14:02 +0200

    Set path

--- scripts/code_coverage.sh
+++ scripts/code_coverage.sh
@@ -3,9 +3,8 @@
 # SPDX-License-Identifier: CC0-1.0
 
 DBS_ONBOARD_MAP_API_DIR=`pwd`
-# COVERAGE_DIR=$DBS_ONBOARD_MAP_API_DIR/build/coverage
-COVERAGE_DIR=$DBS_ONBOARD_MAP_API_DIR
+COVERAGE_DIR=$DBS_ONBOARD_MAP_API_DIR/build/coverage
 EXCLUDE='.*tests/|.*externals/|.*build/|.*download/'
 
-# rm -rf $COVERAGE_DIR && mkdir -p $COVERAGE_DIR
+rm -rf $COVERAGE_DIR && mkdir -p $COVERAGE_DIR
 gcovr -r $DBS_ONBOARD_MAP_API_DIR --exclude $EXCLUDE --html --html-details -o $COVERAGE_DIR/coverage.html
