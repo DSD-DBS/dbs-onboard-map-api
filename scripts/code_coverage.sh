8aaf5701a72bdcc0f92bbf2e6429644b8a9f3947
vladimir-b <vladimir.bezkorovainy@gmail.com>
2023-07-10 23:55:15 +0200

    Upload code coverage to Codecov app

--- scripts/code_coverage.sh
+++ scripts/code_coverage.sh
@@ -7,4 +7,5 @@ COVERAGE_DIR=$DBS_ONBOARD_MAP_API_DIR/build/coverage
 EXCLUDE='.*tests/|.*externals/|.*build/|.*download/'
 
 rm -rf $COVERAGE_DIR && mkdir -p $COVERAGE_DIR
-gcovr -r $DBS_ONBOARD_MAP_API_DIR --exclude $EXCLUDE --html --html-details -o $COVERAGE_DIR/coverage.html
+# gcovr -r $DBS_ONBOARD_MAP_API_DIR --exclude $EXCLUDE --html --html-details -o $COVERAGE_DIR/coverage.html
+gcovr -r $DBS_ONBOARD_MAP_API_DIR --exclude $EXCLUDE --xml $COVERAGE_DIR/coverage.xml
