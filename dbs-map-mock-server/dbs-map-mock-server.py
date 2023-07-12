0be6ee3646978e0ba6c98d08ae7b3e4f33fdfc5b
Volodymyr Bezkorovainyi <vladimir.bezkorovainy@gmail.com>
2023-07-12 22:13:24 +0200

    mock all REST api except "get all blobs"

--- dbs-map-mock-server/dbs-map-mock-server.py
+++ dbs-map-mock-server/dbs-map-mock-server.py
@@ -17,6 +17,57 @@ def get_catalog_matadata_with_version(match):
     catalogVersion = match.group('catalogVersion')
     return path.format(catalogId, catalogVersion)
 
+def get_layer_matadata(match):
+    path = "./hdmap/{}/current/{}/metadata.json"
+    catalogId = match.group('catalogId')
+    layerId = match.group('layerId')
+    return path.format(catalogId, layerId)
+
+def get_catalog_matadata_with_version(match):
+    path = "./hdmap/{}/{}/{}/metadata.json"
+    catalogId = match.group('catalogId')
+    layerId = match.group('layerId')
+    catalogVersion = match.group('catalogVersion')
+    return path.format(catalogId, catalogVersion, layerId)
+
+# def get_layer_partitions(match):
+#     path = "./hdmap/{}/current/{}/partitions/{}_metadata.json"
+#     catalogId = match.group('catalogId')
+#     layerId = match.group('layerId')
+#     partitionId = match.group('partitionId')
+#     return path.format(catalogId, layerId, partitionId)
+
+# def get_layer_partitions_with_version(match):
+#     path = "./hdmap/{}/{}/{}/partitions/{}_metadata.json"
+#     catalogId = match.group('catalogId')
+#     layerId = match.group('layerId')
+#     partitionId = match.group('partitionId')
+#     catalogVersion = match.group('catalogVersion')
+#     return path.format(catalogId, catalogVersion, layerId, partitionId)
+
+def get_partition_metadata(match):
+    path = "./hdmap/{}/current/{}/partitions/{}_metadata.json"
+    catalogId = match.group('catalogId')
+    layerId = match.group('layerId')
+    partitionId = match.group('partitionId')
+    return path.format(catalogId, layerId, partitionId)
+
+def get_partition_metadata_with_version(match):
+    path = "./hdmap/{}/{}/{}/partitions/{}_metadata.json"
+    catalogId = match.group('catalogId')
+    layerId = match.group('layerId')
+    partitionId = match.group('partitionId')
+    catalogVersion = match.group('catalogVersion')
+    return path.format(catalogId, catalogVersion, layerId, partitionId)
+
+def get_partition_data_with_version(match):
+    path = "./hdmap/{}/{}/{}/partitions/{}"
+    catalogId = match.group('catalogId')
+    layerId = match.group('layerId')
+    partitionId = match.group('partitionId')
+    catalogVersion = match.group('catalogVersion')
+    return path.format(catalogId, catalogVersion, layerId, partitionId)
+
 def match_to_path(self):
     # Match to GET /catalogs/{catalogId}
     pattern = r"^/catalogs/(?P<catalogId>[^/?]+)$"
@@ -29,6 +80,48 @@ def match_to_path(self):
     match = re.search(pattern, self.path)
     if match:
         return get_catalog_matadata_with_version(match)
+
+    # Match to GET /catalogs/{catalogId}/layers/{layerId}
+    pattern = r"/catalogs/(?P<catalogId>[^/]+)/layers/(?P<layerId>[^/]+)"
+    match = re.search(pattern, self.path)
+    if match:
+        return get_layer_matadata(match)
+
+    # Match to GET /catalogs/{catalogId}/layers/{layerId}?catalogVersion=
+    pattern = r"/catalogs/(?P<catalogId>[^/]+)/layers/(?P<layerId>[^/]+)\?catalogVersion=(?P<catalogVersion>\d+)"
+    match = re.search(pattern, self.path)
+    if match:
+        return get_layer_matadata_with_version(match)
+
+    # Match to /catalogs/{catalogId}/layers/{layerId}/blobs
+    # pattern = r"/catalogs/(?P<catalogId>[^/]+)/layers/(?P<layerId>[^/]+)/blobs"
+    # match = re.search(pattern, self.path)
+    #     if match:
+    #         return get_layer_partitions(match)
+
+    # Match to /catalogs/{catalogId}/layers/{layerId}/blobs?catalogVersion=
+    # pattern = r"/catalogs/(?P<catalogId>[^/]+)/layers/(?P<layerId>[^/]+)/blobs\?catalogVersion=(?P<catalogVersion>\d+)"
+    # match = re.search(pattern, self.path)
+    #     if match:
+    #         return get_layer_partitions_with_version(match)
+
+    # Match to /catalogs/{catalogId}/layers/{layerId}/blobs/{blobKey}
+    pattern = r"/catalogs/(?P<catalogId>[^/]+)/layers/(?P<layerId>[^/]+)/blobs/(?P<blobKey>[^/]+)"
+    match = re.search(pattern, self.path)
+    if match:
+        return get_partition_matadata(match)
+
+    # Match to /catalogs/{catalogId}/layers/{layerId}/blobs/{blobKey}?catalogVersion=
+    pattern = r"/catalogs/(?P<catalogId>[^/]+)/layers/(?P<layerId>[^/]+)/blobs/(?P<blobKey>[^/]+)\?catalogVersion=(?P<catalogVersion>\d+)"
+    match = re.search(pattern, self.path)
+    if match:
+        return get_partition_matadata_with_version(match)
+
+    # Match to /blob/{catalogId}/{catalogVersion}/{layerId}/{blobKey}
+    pattern = r"/blob/(?P<catalogId>[^/]+)/(?P<catalogVersion>\d+)/(?P<layerId>[^/]+)/(?P<blobKey>[^/]+)"
+    match = re.search(pattern, self.path)
+    if match:
+        return get_partition_data_with_version(match)
 
 class CustomHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
 
