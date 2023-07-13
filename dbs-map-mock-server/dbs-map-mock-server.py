8038eaf851186a88d4f4bcc5300f5e36896e592a
Volodymyr Bezkorovainyi <vladimir.bezkorovainy@gmail.com>
2023-07-13 16:45:15 +0200

    Use Flask for mock server

--- dbs-map-mock-server/dbs-map-mock-server.py
+++ dbs-map-mock-server/dbs-map-mock-server.py
@@ -1,12 +1,12 @@
 # SPDX-FileCopyrightText: Copyright DB Netz AG
 # SPDX-License-Identifier: Apache-2.0
 
-import http.server
 import json
 import os
-import re
-import socketserver
 
+from flask import Flask, request, send_file
+
+app = Flask(__name__)
 
 def find_blobkey_by_datahandle(directory, data_handle):
     for root, dirs, files in os.walk(directory):
@@ -22,45 +22,37 @@ def find_blobkey_by_datahandle(directory, data_handle):
                         print(f"File {filepath} is not a valid JSON file.")
     return None
 
-def get_catalog_matadata(match):
+def version(request):
+    return request.args.get('catalogVersion', 'current')
+
+@app.route('/catalogs/<catalog_id>', methods=['GET'])
+def get_catalog_matadata(catalog_id):
     path = './hdmap/{}/{}/metadata.json'
-    return path.format(
-        match.group('catalogId'),
-        match.groupdict().get('catalogVersion', 'current'))
+    return send_file(path.format(catalog_id, version(request)))
 
-def get_layer_matadata(match):
+@app.route('/catalogs/<catalog_id>/layers/<layer_id>', methods=['GET'])
+def get_layer_matadata(catalog_id, layer_id):
     path = "./hdmap/{}/{}/{}/metadata.json"
-    return path.format(
-        match.group('catalogId'),
-        match.groupdict().get('catalogVersion', 'current'),
-        match.group('layerId'))
+    return send_file(path.format(catalog_id, version(request), layer_id))
 
-def get_all_blobs_metadata(match):
+@app.route('/catalogs/<catalog_id>/layers/<layer_id>/blobs', methods=['GET'])
+def get_all_blobs_metadata(catalog_id, layer_id):
     path = "./hdmap/{}/{}/{}/blobs.json"
-    return path.format(
-        match.group('catalogId'),
-        match.groupdict().get('catalogVersion', 'current'),
-        match.group('layerId'))
+    return send_file(path.format(catalog_id, version(request), layer_id))
 
-def get_partition_metadata(match):
+@app.route('/catalogs/<catalog_id>/layers/<layer_id>/blobs/<blob_key>', methods=['GET'])
+def get_partition_metadata(catalog_id, layer_id, blob_key):
     path = "./hdmap/{}/{}/{}/partitions/{}_metadata.json"
-    return path.format(
-        match.group('catalogId'),
-        match.groupdict().get('catalogVersion', 'current'),
-        match.group('layerId'),
-        match.group('blobKey'))
+    return send_file(path.format(catalog_id, version(request), layer_id, blob_key))
 
-def get_partition_data(match):
+@app.route('/blob/catalogs/<catalog_id>/layers/<layer_id>/data/<data_handle>', methods=['GET'])
+def get_partition_data(catalog_id, layer_id, data_handle):
     path = "./hdmap/{}/{}/{}/partitions/"
 
-    partitions_folder = path.format(
-        match.group('catalogId'),
-        match.groupdict().get('catalogVersion', 'current'),
-        match.group('layerId'))
+    partitions_folder = path.format(catalog_id, version(request), layer_id)
 
-    file_name = find_blobkey_by_datahandle( partitions_folder, match.group('dataHandle') )
-    return partitions_folder + file_name if file_name else None
-
+    file_name = find_blobkey_by_datahandle( partitions_folder, data_handle)
+    return send_file(partitions_folder + file_name if file_name else None)
 
 # /catalogs/{catalogId}
 # /catalogs/{catalogId}/layers/{layerId}
@@ -68,48 +60,6 @@ def get_partition_data(match):
 # /catalogs/{catalogId}/layers/{layerId}/blobs/{blobKey}
 # /blob/catalogs/{catalogId}/layers/{layerId}/data/{dataHandle}
 
-# regex pattern to match optional catalogVersion query parameter
-optional_version = r"(?:\?catalogVersion=\d+)?$"
 
-# array of regex patterns and handlers
-route_handlers = [
-    # /catalogs/{catalogId}
-    (re.compile(rf"^/catalogs/(?P<catalogId>[^/?]+){optional_version}"), get_catalog_matadata),
-
-    # /catalogs/{catalogId}/layers/{layerId}
-    (re.compile(rf"^/catalogs/(?P<catalogId>[^/?]+)/layers/(?P<layerId>[^/?]+){optional_version}"), get_layer_matadata),
-
-    # /catalogs/{catalogId}/layers/{layerId}/blobs
-    (re.compile(rf"^/catalogs/(?P<catalogId>[^/?]+)/layers/(?P<layerId>[^/?]+)/blobs{optional_version}"), get_all_blobs_metadata),
-
-    # /catalogs/{catalogId}/layers/{layerId}/blobs/{blobKey}
-    (re.compile(rf"^/catalogs/(?P<catalogId>[^/?]+)/layers/(?P<layerId>[^/?]+)/blobs/(?P<blobKey>[^/?]+){optional_version}"), get_partition_metadata),
-
-    # /blob/catalogs{catalogId}/layers/{layerId}/data/{dataHandle}
-    (re.compile(rf"^/blob/catalogs/(?P<catalogId>[^/?]+)/layers/(?P<layerId>[^/?]+)/data/(?P<dataHandle>[^/?]+){optional_version}"), get_partition_data)
-]
-
-def route_to_file(self):
-    for pattern, handler in route_handlers:
-        match = pattern.match(self.path)
-        print(pattern)
-        if match:
-            print('matched')
-            return handler(match)
-    return None
-
-class CustomHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        print("GET: ", self.path)
-        file_path = route_to_file(self)
-        if file_path:
-            self.path = file_path
-            print("Path to file: ", self.path)
-
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
-
-Handler = CustomHttpRequestHandler
-
-with socketserver.TCPServer(("localhost", 8080), Handler) as httpd:
-    print("serving at port", 8080)
-    httpd.serve_forever()
+if __name__ == '__main__':
+    app.run(port=8080)
